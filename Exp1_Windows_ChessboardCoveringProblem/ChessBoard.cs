5fd6d9ea483161e221d3e0e1a4f7743b79e6ad25
wolfogre <wolfogre@qq.com>
2015-09-06 21:38:04 +0800

    solve Exp1 in C#

---
+++ Exp1_Windows_ChessboardCoveringProblem/ChessBoard.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exp1_Windows_ChessboardCoveringProblem
+{
+	class ChessBoard
+	{
+		int _size;
+		int[,] _board;
+		int _specialX;
+		int _specialY;
+		public ChessBoard(int size, int specialX, int specialY)
+		{
+			_size = size;
+			_specialX = specialX;
+			_specialY = specialY;
+			_board = new int[_size, _size];
+		}
+		public int[,] FillBoard()
+		{
+			int dominoNumber = 1;
+			FillBoardHelp(0, 0, _size, _specialX, _specialY, ref dominoNumber);
+			return (int[,]) _board.Clone();
+		}
+		public void FillBoardHelp(int originRow, int originCol, int originSize, int originSpecialX, int originSpecialY, ref int originNumber)
+		{
+			if (originSize == 1)
+				return;
+			int size = originSize / 2;
+			int number = originNumber++;
+
+			if (originSpecialX < originCol + size && originSpecialY < originRow + size)
+			{
+				FillBoardHelp(originRow, originCol, size, originSpecialX, originSpecialY, ref originNumber);
+			}
+			else
+			{
+				_board[originRow + size - 1, originCol + size - 1] = number;
+				FillBoardHelp(originRow, originCol, size, originCol + size - 1, originRow + size - 1, ref originNumber);
+			}
+
+			if (originSpecialX >= originCol + size && originSpecialY < originRow + size)
+			{
+				FillBoardHelp(originRow, originCol + size, size, originSpecialX, originSpecialY, ref originNumber);
+			}
+			else
+			{
+				_board[originRow + size - 1, originCol + size] = number;
+				FillBoardHelp(originRow, originCol + size, size, originCol + size, originRow + size - 1, ref originNumber);
+			}
+
+			if (originSpecialX < originCol + size && originSpecialY >= originRow + size)
+			{
+				FillBoardHelp(originRow + size, originCol, size, originSpecialX, originSpecialY, ref originNumber);
+			}
+			else
+			{
+				_board[originRow + size, originCol + size - 1] = number;
+				FillBoardHelp(originRow + size, originCol, size, originCol + size - 1, originRow + size, ref originNumber);
+			}
+
+			if (originSpecialX >= originCol + size && originSpecialY >= originRow + size)
+			{
+				FillBoardHelp(originRow + size, originCol + size, size, originSpecialX, originSpecialY, ref originNumber);
+			}
+			else
+			{
+				_board[originRow + size, originCol + size] = number;
+				FillBoardHelp(originRow + size, originCol + size, size, originCol + size, originRow + size, ref originNumber);
+			}
+		}
+	}
+}
---
+++ Exp1_Windows_ChessboardCoveringProblem/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exp1_Windows_ChessboardCoveringProblem
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			int size = 4;
+			ChessBoard cb = new ChessBoard(size, 2, 1);
+			int [,] result = cb.FillBoard();
+			textBox1.Clear();
+			for(int i = 0; i < size; ++i)
+			{
+				for (int j = 0; j < size; ++j)
+					textBox1.Text += " " + result[i, j];
+				textBox1.Text += " " + Environment.NewLine;
+			}
+		}
+	}
+}
