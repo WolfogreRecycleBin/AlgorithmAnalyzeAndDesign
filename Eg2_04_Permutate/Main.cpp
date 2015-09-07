e0ce2ec854141a1d8ab84f1d14fcefd3db4c2757
wolfogre <wolfogre@qq.com>
2015-09-07 10:54:58 +0800

    come on,.. the Main.cpp was forgotten

---
+++ Eg2_04_Permutate/Main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+
+template<class Type>
+inline void Swap(Type & a, Type & b)
+{
+	Type temp = a;
+	a = b;
+	b = temp;
+}
+
+template<class Type>
+void Perm(Type list[], int k, int m)
+{
+	if (k == m)
+	{
+		for (int i = 0; i <= m; ++i)
+			cout << list[i] << " ";
+		cout << endl;
+	}
+	else
+	{
+		for (int i = k; i <= m; ++i)
+		{
+			Swap(list[k], list[i]);
+			Perm(list, k + 1, m);
+			Swap(list[i], list[k]);
+		}
+	}
+}
+
+int main()
+{
+	int list[] = { 1, 2, 3, };
+	Perm(list, 0, 2);
+	system("pause");
+	return 0;
+}
