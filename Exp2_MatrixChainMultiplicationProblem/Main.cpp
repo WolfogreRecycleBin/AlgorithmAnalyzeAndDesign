79c60ec9aa8a6fa1d48b86565c00f12e71a35ca8
wolfogre <wolfogre@qq.com>
2015-09-19 13:50:58 +0800

    adapt to the input and output format

--- Exp2_MatrixChainMultiplicationProblem/Main.cpp
+++ Exp2_MatrixChainMultiplicationProblem/Main.cpp
@@ -39,7 +39,7 @@ string Traceback(int i, int j, int **s, string left_str, string right_str)
 		_itoa_s(i, str, 10);
 		return "A" + string(str);
 	}
-		
+
 	left_str += Traceback(i, s[i][j], s, "", "");
 	right_str += Traceback(s[i][j] + 1, j, s, "", "");
 	return "(" + left_str + right_str + ")";
@@ -47,19 +47,39 @@ string Traceback(int i, int j, int **s, string left_str, string right_str)
 
 int main()
 {
-	int p[7] = { 30, 35, 15, 5, 10, 20 , 25};
-	int **m = new int*[7];
-	int **s = new int*[7];
-	for (int i = 0; i < 7; ++i)
+	int p[20];
+	int **m = new int*[20];
+	int **s = new int*[20];
+	for (int i = 0; i < 20; ++i)
 	{
-		m[i] = new int[7];
-		s[i] = new int[7];
+		m[i] = new int[20];
+		s[i] = new int[20];
 	}
-	MatrixChain(p, 6, m, s);
-	string result = Traceback(1, 6, s, "", "");
-	result.erase(0, 1);
-	result.erase(result.length() - 1, 1);
-	cout << result;
+
+	int n;
+	int count = 0;
+	while (cin >> n)
+	{
+		++count;
+		for (int i = 0; i <= n; ++i)
+		{
+			cin >> p[i];
+		}
+		MatrixChain(p, n, m, s);
+		string result = Traceback(1, n, s, "", "");
+		result.erase(0, 1);
+		result.erase(result.length() - 1, 1);
+		cout << "Case " << count << endl;
+		cout << m[1][n] << " " << result << endl;
+	}
+
+	for (int i = 0; i < 20; ++i)
+	{
+		delete[] m[i];
+		delete[] s[i];
+	}
+	delete[] m;
+	delete[] s;
 	system("pause");
 	return 0;
 }
