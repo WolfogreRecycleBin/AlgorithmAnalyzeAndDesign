4b7424686bcd1484c5f0a569339012f110f6f9b5
wolfogre <wolfogre@qq.com>
2015-10-11 17:05:55 +0800

    Got a new way

--- Exp4_HuffmanCoding/Main.cpp
+++ Exp4_HuffmanCoding/Main.cpp
@@ -1,16 +1,57 @@
+#include <iostream>
 #include "Node.h"
-#include <vector>
+using namespace std;
+
 int main(void)
 {
 	
-	int value[] = {9, 8, 3, 4, 1, 2};
-
+	Node<int>  **trees = new Node<int> *[100];
+	int value[] = { 1, 2, 3, 4, 5, 6 };
 	for (int i = 0; i < 6; ++i)
+		trees[i] = new Node<int>(value[i]);
+	for (int i = 1; i <= 5; ++i)
 	{
-		//I believe there is a hole, a big hole.
+		Node<int> *firstMin = NULL;
+		Node<int> *secondMin = NULL;
+		int firstMinIndex = -1;
+		int secondMinIndex = -1;
+		for (int index = 0; index < 6; ++index)
+		{
+			if (trees[index] != NULL)
+			{
+				if (secondMin == NULL ||  trees[index]->data < secondMin->data)
+				{
+					secondMin = trees[index];
+					secondMinIndex = index;
+				}
+				if (firstMin == NULL || secondMin->data < firstMin->data)
+				{
+					Node<int> *temp = firstMin;
+					int tempIndex = firstMinIndex;
+					firstMin = secondMin;
+					firstMinIndex = secondMinIndex;
+					secondMin = temp;
+					secondMinIndex = tempIndex;
+				}
+			}
+		}
+		if (firstMinIndex > secondMinIndex)
+		{
+			Node<int> *temp = firstMin;
+			int tempIndex = firstMinIndex;
+			firstMin = secondMin;
+			firstMinIndex = secondMinIndex;
+			secondMin = temp;
+			secondMinIndex = tempIndex;
+		}
+		Node<int> *newNode = new Node<int>(firstMin->data + secondMin->data);
+		newNode->leftChild = firstMin;
+		newNode->rightChild = secondMin;
+		trees[firstMinIndex] = newNode;
+		trees[secondMinIndex] = NULL;
 	}
-
-
+	
+	system("pause");
 	return 0;
 }
 
--- Exp4_HuffmanCoding/Node.h
+++ Exp4_HuffmanCoding/Node.h
@@ -7,27 +7,15 @@ struct Node
 {
 // 数据成员:
 	ElemType data;				// 数据域
-	Node<ElemType> *next;		// 指针域
+	Node<ElemType> *leftChild;
+	Node<ElemType> *rightChild;
 
 // 构造函数:
-	Node();						// 无参数的构造函数
-	Node(ElemType item, Node<ElemType> *link = NULL);	// 已知数数据元素值和指针建立结构
+	Node(ElemType newDate)
+	{
+		leftChild = rightChild = NULL;
+		data = newDate;
+	}
 };
-
-// 结点类的实现部分
-template<class ElemType>
-Node<ElemType>::Node()
-// 操作结果：构造指针域为空的结点
-{
-   next = NULL;
-}
-
-template<class ElemType>
-Node<ElemType>::Node(ElemType item, Node<ElemType> *link)
-// 操作结果：构造一个数据域为item和指针域为link的结点
-{
-   data = item;
-   next = link;
-}
 
 #endif
