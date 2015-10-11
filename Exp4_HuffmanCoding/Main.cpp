c941fe637a7d838a8e0ce257966ce05569e5b26a
wolfogre <wolfogre@qq.com>
2015-10-12 00:32:06 +0800

    Finished Exp4

--- Exp4_HuffmanCoding/Main.cpp
+++ Exp4_HuffmanCoding/Main.cpp
@@ -1,106 +1,99 @@
 #include <iostream>
 #include <vector>
+#include <queue>
+#include <string>
+#include <algorithm>//for sort()
+#include <functional>//for greater<int>()
 #include "Node.h"
 using namespace std;
 
-void ShowTree(Node<int>  *root)
+void CalHuffmanCode(Node<int>  *root)
 {
-	if (root == NULL)
-		return;
-	ShowTree(root->leftChild);
-	cout << root->data << " ";
-	ShowTree(root->rightChild);
-}
-
-void ShowHuffmanCode(Node<int>  *root, vector<int> &code)
-{
-	bool isLeef = true;
 	if (root->leftChild != NULL)
 	{
-		isLeef = false;
-		code.push_back(0);
-		ShowHuffmanCode(root->leftChild, code);
-		code.pop_back();
+		root->leftChild->huffmanCode = root->huffmanCode + "0";
+		CalHuffmanCode(root->leftChild);
 	}
 	if (root->rightChild != NULL)
 	{
-		isLeef = false;
-		code.push_back(1);
-		ShowHuffmanCode(root->rightChild, code);
-		code.pop_back();
+		root->rightChild->huffmanCode = root->huffmanCode + "1";
+		CalHuffmanCode(root->rightChild);
 	}
-	if (isLeef)
+}
+
+void ShowHuffmanCode(Node<int> *root)
+{
+	CalHuffmanCode(root);
+	queue<Node<int> *> nodeQueue;
+	nodeQueue.push(root);
+	while (!nodeQueue.empty())
 	{
-		cout << root->data << " ";
-		for (auto it : code)
-			cout << it;
-		cout << endl;
+		Node<int> * node = nodeQueue.front();
+		nodeQueue.pop();
+		bool isLeef = true;
+		if (node->leftChild != NULL)
+		{
+			isLeef = false;
+			nodeQueue.push(node->leftChild);
+		}
+		if (node->rightChild != NULL)
+		{
+			isLeef = false;
+			nodeQueue.push(node->rightChild);
+		}
+		if (isLeef)
+		{
+			cout << node->data << " " << node->huffmanCode << endl;
+		}
 	}
+}
+
+bool Greater(Node<int>* a, Node<int>* b)
+{
+	return a->data > b->data;
 }
 
 int main(void)
 {
+	int caseNum;
+	cin >> caseNum;
+	int caseCount = 0;
+	while (++caseCount <= caseNum)
+	{
+		vector<Node<int>*> trees;
+		int valueCount;
+		cin >> valueCount;
+		while (--valueCount >= 0)
+		{
+			int value;
+			cin >> value;
+			trees.push_back(new Node<int>(value));
+		}
+		
+		sort(trees.begin(), trees.end(), Greater);
+
+		while (trees.size() > 1)
+		{
+			Node<int>* rightTree = trees.back();
+			trees.pop_back();
+			Node<int>* leftTree = trees.back();
+			trees.pop_back();
+			Node<int> *newNode = new Node<int>(leftTree->data + rightTree->data);
+			newNode->leftChild = leftTree;
+			newNode->rightChild = rightTree;
+			vector<Node<int>*>::iterator it;
+			for (it = trees.begin(); it != trees.end(); ++it)
+				if ((*it)->data < newNode->data)
+					break;
+			trees.insert(it, newNode);
+		}
+
+		cout << "Case " << caseCount << endl;
+		ShowHuffmanCode(trees.back());
+		cout << endl;
+	}
 	
-	Node<int>  **trees = new Node<int> *[100];
-	int value[] = { 1, 2, 3, 4, 5, 6 };
-	for (int i = 0; i < 6; ++i)
-		trees[i] = new Node<int>(value[i]);
-	for (int i = 1; i <= 5; ++i)
-	{
-		Node<int> *firstMin = NULL;
-		Node<int> *secondMin = NULL;
-		int firstMinIndex = -1;
-		int secondMinIndex = -1;
-		for (int index = 0; index < 6; ++index)
-		{
-			if (trees[index] != NULL)
-			{
-				if (secondMin == NULL ||  trees[index]->data < secondMin->data)
-				{
-					secondMin = trees[index];
-					secondMinIndex = index;
-				}
-				if (firstMin == NULL || secondMin->data < firstMin->data)
-				{
-					Node<int> *temp = firstMin;
-					int tempIndex = firstMinIndex;
-					firstMin = secondMin;
-					firstMinIndex = secondMinIndex;
-					secondMin = temp;
-					secondMinIndex = tempIndex;
-				}
-			}
-		}
-		if (firstMinIndex > secondMinIndex)
-		{
-			Node<int> *temp = firstMin;
-			int tempIndex = firstMinIndex;
-			firstMin = secondMin;
-			firstMinIndex = secondMinIndex;
-			secondMin = temp;
-			secondMinIndex = tempIndex;
-		}
-		//cout << firstMin->data << " " << secondMin->data << endl;
-		Node<int> *newNode = new Node<int>(firstMin->data + secondMin->data);
-		newNode->leftChild = firstMin;
-		newNode->rightChild = secondMin;
-		trees[firstMinIndex] = newNode;
-		trees[secondMinIndex] = NULL;
-	}
-	//for (int i = 0; i < 6; ++i)
-	//{
-	//	ShowTree(trees[i]);
-	//	cout << endl;
-	//}
-	for (int i = 0; i < 6; ++i)
-	{
-		if (trees[i] != NULL)
-		{
-			vector<int> code;
-			ShowHuffmanCode(trees[i], code);
-			break;
-		}
-	}
+	
 	system("pause");
 	return 0;
 }
--- Exp4_HuffmanCoding/Node.h
+++ Exp4_HuffmanCoding/Node.h
@@ -1,6 +1,6 @@
 #ifndef __NODE_H__
 #define __NODE_H__
-
+#include <string>
 // 结点类
 template <class ElemType>
 struct Node 
@@ -9,7 +9,7 @@ struct Node
 	ElemType data;				// 数据域
 	Node<ElemType> *leftChild;
 	Node<ElemType> *rightChild;
-
+	std::string huffmanCode;
 // 构造函数:
 	Node(ElemType newDate)
 	{
