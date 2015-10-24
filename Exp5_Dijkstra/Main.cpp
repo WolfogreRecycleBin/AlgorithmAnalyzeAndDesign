1d8f6211d55f34e5920bd7cb525a28bfde872064
wolfogre <wolfogre@qq.com>
2015-10-24 21:32:12 +0800

    Finished Exp5

--- Exp5_Dijkstra/Main.cpp
+++ Exp5_Dijkstra/Main.cpp
@@ -1,52 +1,89 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-#define MAX_NUM 1000
+int mat[1000][1000];
+int nodeCount;
+vector<int> bestWay;
+vector<int> way;
+int bestWayLenth;
+int wayLenth;
 
-void Dijkstra(int nodeNum, int startNode, int dist[], int prev[], int **matrix)
+bool IsInWay(int node)
 {
-	bool s[MAX_NUM];
-	for (int i = 1; i <= nodeNum; ++i)
+	for (auto it : way)
+		if (it == node)
+			return true;
+	return false;
+}
+
+
+void FindWay(int startNode, int aimNode)//startNode and aimNode are numbers start form 0
+{
+	if (startNode == aimNode)
 	{
-		dist[i] = matrix[startNode][i];
-		s[i] = false;
-		if (dist[i] == MAX_NUM)
-			prev[i] = 0;
-		else
-			prev[i] = startNode;
+		if (wayLenth == bestWayLenth && way.size() >= bestWay.size())
+			return;
+		bestWayLenth = wayLenth;
+		bestWay = way;
+		return;
 	}
-	dist[startNode] = 0;
-	s[startNode] = true;
-	for (int i = 1; i < nodeNum; ++i)
+	for (int i = 0; i < nodeCount; ++i)
 	{
-		int temp = MAX_NUM;
-		int u = startNode;
-		for (int j = 1; i <= nodeNum; j++)
+		if (!IsInWay(i) && mat[startNode][i] != -1 && wayLenth + mat[startNode][i] <= bestWayLenth)
 		{
-			if ((!s[j]) && (dist[j] < temp))
-			{
-				u = j;
-				temp = dist[j];
-			}
-			s[u] = true;
-			for (int j = 1; j <= startNode; ++j)
-			{
-				if ((!s[j]) && (matrix[u][j] < MAX_NUM))
-				{
-					int newDist = dist[u] + matrix[u][j];
-					if (newDist < dist[j])
-					{
-						dist[j] = newDist;
-						prev[j] = u;
-					}
-				}
-			}
+			wayLenth += mat[startNode][i];
+			way.push_back(i);
+			FindWay(i, aimNode);
+			way.pop_back();
+			wayLenth -= mat[startNode][i];
 		}
 	}
 }
 
+void Init()
+{
+	bestWay.clear();
+	way.clear();
+	bestWayLenth = INT_MAX;
+	wayLenth = 0;
+}
+
+void ShowBestWay()
+{
+	bool isFirst = true;
+	for (auto it : bestWay)
+	{
+		if (isFirst)
+		{
+			cout << it + 1;
+			isFirst = false;
+			continue;
+		}
+		cout << "->" << it + 1;
+	}
+		
+}
+
 int main()
 {
-	system("pause");
+	int caseCount = 0;
+	while (cin >> nodeCount)
+	{
+		for (int i = 0; i < nodeCount; ++i)
+			for (int j = 0; j < nodeCount; ++j)
+				cin >> mat[i][j];
+		int startNode;
+		int aimNode;
+		cin >> startNode >> aimNode;
+		Init();
+		way.push_back(startNode - 1);
+		FindWay(startNode - 1, aimNode - 1);
+		cout << "Case " << ++caseCount << endl;
+		cout << "The least distance from " << startNode << "->" << aimNode << " is " << bestWayLenth << endl;
+		cout << "the path is ";
+		ShowBestWay();
+		cout << endl;
+	}
 	return 0;
 }
