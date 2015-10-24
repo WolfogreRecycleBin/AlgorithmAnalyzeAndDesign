eae35045e40a8a0280fdd2b5312d1165c57c9b46
wolfogre <wolfogre@qq.com>
2015-10-25 02:05:34 +0800

    New project Exp7_LoadingProblem, submitted

---
+++ Exp7_LoadingProblem/Main.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+vector<int> result;
+vector<int> bestResult;
+int w[10000];
+int boxCount;
+int c1, c2;
+int s1, s2;
+int bestW;
+
+void Backtrack(int index)
+{
+	if (index >= boxCount)
+	{
+		if (s1 > bestW)
+		{
+			bestW = s1;
+			bestResult = result;
+		}
+		return;
+	}
+	if (s1 + w[index] <= c1)
+	{
+		s1 += w[index];
+		result.push_back(1);
+		Backtrack(index + 1);
+		result.pop_back();
+		s1 -= w[index];
+	}
+	if (s2 + w[index] <= c2)
+	{
+		s2 += w[index];
+		result.push_back(0);
+		Backtrack(index + 1);
+		result.pop_back();
+		s2 -= w[index];
+	}
+}
+
+int main()
+{
+	int caseCount = 0;
+
+	while (cin >> boxCount)
+	{
+		for (int i = 0; i < boxCount; ++i)
+			cin >> w[i];
+		cin >> c1 >> c2;
+		result.clear();
+		bestResult.clear();
+		s1 = s2 = bestW = 0;
+		Backtrack(0);
+		
+		cout << "Case " << ++caseCount << endl;
+		if (bestResult.size() == 0)
+			cout << "No" << endl;
+		else
+		{
+			cout << bestW << " ";
+			for (auto it : bestResult)
+				cout << it;
+			cout << endl;
+		}
+	}
+	return 0;
+}
